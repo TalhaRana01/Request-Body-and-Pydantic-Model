683c788ea00e208e8e9be7161dc978548a4590b7
Talha Rana <mtalharana093@gmail.com>
2025-11-20 23:40:11 +0500

    Singular value in defined in request body

--- app/2_multiple_body_parameter.py
+++ app/2_multiple_body_parameter.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from typing import Annotated
 from pydantic import BaseModel
 
 
@@ -22,7 +23,18 @@ class Seller(BaseModel):
 #   return {"product": product, "seller": seller}
 
 # Make body parameter optional
+# @app.post("/product")
+# async def create_product(product: Product, seller: Seller | None = None):
+#   return {"product": product, "seller": seller}
+
+# Singular value in body
+
 @app.post("/product")
-async def create_product(product: Product, seller: Seller | None = None):
-  return {"product": product, "seller": seller}
+async def create_product(
+  product: Product,   # Product Pydentic Schema
+  seller: Seller,     # Seller Pydantic Schema
+  secret_key : Annotated[str, Body()]   # Extra Schema that is not defined in Pydantic Schema
+  ):
+  
+  return {"product": product, "seller": seller, "secret_key": secret_key}
   
