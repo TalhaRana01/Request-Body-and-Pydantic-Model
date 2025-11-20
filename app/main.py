9fdea77e3d76c7fff8fc6816e3cc67cfec11ec95
Talha Rana <mtalharana093@gmail.com>
2025-11-20 18:17:02 +0500

    create route define as a pydantic model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI(
   title="Request Body and Pydantic Model",
   version="0.1.0"
 )
 
+# With Pydantic validation 
+
+class Product(BaseModel):
+  id : int
+  name : str
+  price : float
+  stock : int | None = None   # optional field
+
+
+# Basic create route without request body validation 
 # Create or insert Data
-@app.post("/products")
-async def get_products(new_product : dict):
-  return {"response": "Product created", "new_product": new_product}
+# @app.post("/products")
+# async def get_products(new_product : dict):
+#   """
+#     Ya without pydantic validation ha 
+#     request body main kuch data  hi bhej skty hain
+    
+#   """
+#   return {"response": "Product created", "new_product": new_product}
+
+
+
+@app.post("/products", status_code=status.HTTP_201_CREATED)
+async def create_product(new_product: Product):
+  """
+      Hum ny Pydantic class main 
+      already define kr diya ha es liye humhan 
+      wo fields required hogi agr koi missing hui to error aye ga
+  """
+  return {"message": "Product created", "product": new_product}
+
+
+# {
+#   "id": 1,
+#   "name": "product1",
+#   "price": 25.5,
+#   "stock": 10
+# }
+  
