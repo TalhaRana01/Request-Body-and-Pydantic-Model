a011aec890383112ccd0a4efaf52db47ce461fdd
Talha Rana <mtalharana093@gmail.com>
2025-11-21 17:55:38 +0500

    basic without return types

--- app/5_return_types.py
+++ app/5_return_types.py
@@ -1,50 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
-# without return types
-# @app.get("/products")
-# async def get_products():
-#   return {"resposne" : "fastapi"}
-
-# Without return type
-# @app.post("/products")
-# async def get_product(product: dict):
-#   # return product
-#   return "hello world"
+# Without return type we can send any kind of data 
+# we can return string value
+# we can return JSON value
+#  we can return list values
 
 
-class Product(BaseModel):
-  id : int
-  name: str
-  price: float
- 
-  
-class ProductOut(Product):
-  name: str
-  price : float
-  
-  
-  
-  
-# @app.post("/product")
-# async def get_product(product: Product):
-#   # return product   
-#   # return "hello world"
-  
+# Examples:
 
-# With return type restriction 
-# @app.post("/product")
-# async def create_product(product: Product) -> Product:
-#   # return "hello world" 
-#   return product
-
-@app.post("/product")
-async def create_product(product: list[Product] ):
-  return product
+@app.get("/product")
+async def get_product():
+  # return "hello world" 
+  # return {"response" : "Ok"}
+  return [
+    {"id": 1, "name": "laptop", "price": 25000},
+    {"id": 2, "name": "mobile", "price": 50000},
+    {"id": 3, "name": "mini laptop", "price": 75000}
+  ]
 
   
-# @app.post("/product")
-# async def create_product(product: Product ) -> ProductOut:
-#   return product
